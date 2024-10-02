ebe86b4039e7d8e8aee0ceb0760002936bdc8731
Henrique <h8s5g6@gmail.com>
2024-10-01 22:13:07 -0300

    hasidhaskdh

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,64 +1,33 @@
-package com.example.henriquetextfield
+package com.example.minhaaplicacao
 
 import android.os.Bundle
-import android.os.Parcel
-import android.os.Parcelable
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.henriquetextfield.ui.theme.HenriquetextfieldTheme
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.graphics.Color
+import com.example.minhaaplicacao.ui.theme.MinhaAplicacaoTheme
+import com.example.minhaaplicacao.view.MainView
+import com.example.minhaaplicacao.view.MainViewModel
 
-
-class MainActivity() : ComponentActivity() {
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            var vlrTxtField = rememberSaveable {
-                mutableStateOf("")
-            }
-            Column {
-                TextField(
-                    modifier = Modifier.background(Color.Yellow),
-                    value = vlrTxtField.value,
-                    onValueChange = {
-                        Log.d("NOSSOAPP", it)
-                        vlrTxtField.value = it
-                    })
 
+        val viewModel: MainViewModel = MainViewModel()
+
+        setContent {
+            MinhaAplicacaoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainView(modifier = Modifier.padding(innerPadding), viewModel = viewModel)
+                }
             }
         }
-    }
-}
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-    @Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HenriquetextfieldTheme {
-        Greeting("Android")
     }
 }
